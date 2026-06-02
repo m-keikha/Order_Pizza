de765dbc405380cb0eeff49782ee2a594cb15999
m-keikha <89669648+m-keikha@users.noreply.github.com>
2026-06-02 22:14:18 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-//import components
 import Header from './components/Header';
 import Home from './components/Home';
 import Base from './components/Base';
